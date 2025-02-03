87c8865abd52206cd1ade2945f8067d5cb77eb7f
jatingandhidev <jatin05092001@gmail.com>
2025-02-03 13:54:36 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,29 @@ export const metadata: Metadata = {
     template: '%s',
   },
   description:
-    'A powerful collaborative real-time whiteboard built with cutting-edge technologies like Next.js 15, Clerk, Convex, and LiveBlocks. Create from scratch with features like shape tools (rectangles, ellipses), sticky notes, and freehand drawing. Seamlessly sync and collaborate in real-time with a live database for an interactive and dynamic experience. Perfect for teams, designers, and remote brainstorming',
+    'A collaborative, real-time whiteboard. Packed with features such as real-time database, whiteboard from scratch with ability to add shapes like Rectangles and Ellipses, Sticky notes and Pencil drawing. Using the newest technologies such as Next.js. 14, Clerk, Convex and LiveBlocks.',
+  openGraph: {
+    type: 'website',
+    url: 'https://drawit-salvador.vercel.app/',
+    title: 'Drawit',
+    description:
+      'A collaborative, real-time whiteboard. Packed with features such as real-time database, whiteboard from scratch with ability to add shapes like Rectangles and Ellipses, Sticky notes and Pencil drawing. Using the newest technologies such as Next.js. 14, Clerk, Convex and LiveBlocks.',
+    images: [
+      {
+        url: 'https://metatags.io/images/meta-tags.png',
+        width: 1200,
+        height: 630,
+        alt: 'Drawit preview image',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Drawit',
+    description:
+      'A collaborative, real-time whiteboard. Packed with features such as real-time database, whiteboard from scratch with ability to add shapes like Rectangles and Ellipses, Sticky notes and Pencil drawing. Using the newest technologies such as Next.js. 14, Clerk, Convex and LiveBlocks.',
+    images: ['https://metatags.io/images/meta-tags.png'],
+  },
 }
 
 export default function RootLayout({
